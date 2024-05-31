558cab76903949888e40e5ee86b6406519d4eb88
aniruddhbhat9812 <136747313+aniruddhbhat9812@users.noreply.github.com>
2024-05-31 15:55:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, Blueprint
-from flask_cors import CORS
-
-
+from flask import Flask
 
 app = Flask(__name__)
 
-CORS(app, resources={r"*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]}})
+@app.route('/')
+def home():
+    return "Hello, Azure!"
 
-main_bp = Blueprint("main", __name__)
-
-@main_bp.route("/ping", methods=["GET"])
-def homepage():
-    return "The server responded successfully", 200
-
-app.register_blueprint(main_bp)
-
-if __name__ == "__main__":
-    app.run()
-
-from controller import *
+if __name__ == '__main__':
+    app.run(debug=True)
