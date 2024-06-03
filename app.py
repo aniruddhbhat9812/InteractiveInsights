17f2fd6597968f626d02043f5cace13f96068930
aniruddhbhat9812 <136747313+aniruddhbhat9812@users.noreply.github.com>
2024-06-03 15:12:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_cors import CORS
+# from controller import *
+import os
+# from app import app
+from flask import request, jsonify, session
+from flask_cors import CORS
+from model.campaign_model import InteractiveInsightsDTO
+from services.processing_campaign import genAIOutput
+import logging
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Azure!"
+CORS(app, resources={r"*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]}})
 
-if __name__ == '__main__':
+main_bp = Blueprint("main", __name__)
+
+@main_bp.route("/ping", methods = ['GET'])
+def home():
+    return "The server responded successfully", 200
+
+app.register_blueprint(main_bp)
+
+logging.basicConfig(level=logging.INFO)
+
+@app.route('/InteractiveInsights', methods=['POST'])
+def InteractiveInsights_Output():
+    try:
+        body = request.json
+        print("Step1 Successful")
+        if InteractiveInsightsDTO.validatePayload(body):
+            q = InteractiveInsightsDTO.instance_from_flask_body(body)
+            logging.info(q)
+            print(body)
+            result = genAIOutput(body["query"])
+            if 'error' in result:
+                return jsonify(result), 500
+            return jsonify(result), 200
+    except Exception as e:
+        return jsonify({'error': f'{str(e)}'}), 503
+        
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+from controller import user_controller
+
